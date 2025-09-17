a85fde10ebb36f5d1b09718c1c406e18bee6ba01
inzamulhaque <mdihalif@gmail.com>
2025-09-18 01:42:51 +0600

    graphQL type

---
+++ src/schema.ts
@@ -0,0 +1,33 @@
+const typeDefs = `#graphql
+  
+  type Post {
+    id: ID!
+    title: String!
+    content: String!
+    published: Boolean!
+    author: User
+    createdAt: String
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    createdAt: String!
+    posts: [Post]
+  }
+
+  type Profile {
+        id: ID!
+        bio: String!
+        createdAt: String!
+        user: User!
+    }
+
+  type Query {
+    me: User
+    posts: [Post]
+  }
+`;
+
+export default typeDefs;
