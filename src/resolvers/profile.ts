f48e39c2365bbc392c951dcb3274587451ab0e2b
inzamulhaque <mdihalif@gmail.com>
2025-10-08 00:37:06 +0600

    established relations between profile and user

---
+++ src/resolvers/profile.ts
@@ -0,0 +1,9 @@
+const Profile = {
+  user: async (parent: any, args: any, { prisma, userInfo }: any) => {
+    return await prisma.user.findUnique({
+      where: { id: parent.userId },
+    });
+  },
+};
+
+export default Profile;
